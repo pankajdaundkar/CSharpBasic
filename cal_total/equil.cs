39c20c05e618ae9caa5536f74741b53408720913
pankajdaundkar <phd27993@gmail.com>
2023-06-09 15:51:25 +0530

    add program

---
+++ cal_total/equil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cal_total
+{
+    internal class equil
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area of an equivalent triangle");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            double area = 1.73 / 4 * (a) * 2;
+
+            Console.WriteLine(area);
+        }
+    }
+}
